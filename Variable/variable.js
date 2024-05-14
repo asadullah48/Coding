90c99d271fa41d6f04a2da1af2afb353003d26ea
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-05-14 11:03:30 +0500

    added new file

---
+++ Variable/variable.js
@@ -0,0 +1,8 @@
+var userName = "asadullahshafique";
+var age = 48;
+var isStudent = true;
+var randomValue = "https://www.linkedin.com/posts/asadullah48coding-all-45-answers-actitvity-719601859931-ws_o?utm_source=share&utm_medium=member_desktop";
+console.log(userName);
+console.log(age);
+console.log(isStudent);
+console.log(randomValue);
---
+++ Variable/variable.ts
@@ -0,0 +1,8 @@
+let userName: string = "asadullahshafique";
+let age: number = 48;
+let isStudent: boolean = true;
+let randomValue: any = "https://www.linkedin.com/posts/asadullah48coding-all-45-answers-actitvity-719601859931-ws_o?utm_source=share&utm_medium=member_desktop";
+console.log(userName);
+console.log(age);
+console.log(isStudent);
+console.log(randomValue);
