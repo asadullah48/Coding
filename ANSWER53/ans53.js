9a5b17a611cf6522679f74c123640fc080b8f061
asadullah48 <asadullahshafique@hotmail.com>
2024-05-20 10:21:13 +0500

    remove error

--- ANSWER53/ans53.js
+++ ANSWER53/ans53.js
@@ -8,3 +8,7 @@ var developerSkills = {
 var languages = developerSkills.languages, frameworks = developerSkills.frameworks, tools = developerSkills.tools;
 // Showing a skill from each category
 console.log("Language: ".concat(languages[0], ", Framework: ").concat(frameworks[0], ", Tool: ").concat(tools[0]));
+console.log("Language: ".concat(languages[1], ", Framework: ").concat(frameworks[1], ", Tool: ").concat(tools[1]));
+console.log("Language: ".concat(languages[2], ", Framework: ").concat(frameworks[2], ", Tool: ").concat(tools[2]));
+console.log("Language: ".concat(languages[2], ", Framework: ").concat(frameworks[0], ", Tool: ").concat(tools[1]));
+console.log("Language: ".concat(languages[1], ", Framework: ").concat(frameworks[0], ", Tool: ").concat(tools[0]));
--- ANSWER53/ans53.ts
+++ ANSWER53/ans53.ts
@@ -10,3 +10,7 @@ let { languages, frameworks, tools } = developerSkills;
 
 // Showing a skill from each category
 console.log(`Language: ${languages[0]}, Framework: ${frameworks[0]}, Tool: ${tools[0]}`);
+console.log(`Language: ${languages[1]}, Framework: ${frameworks[1]}, Tool: ${tools[1]}`);
+console.log(`Language: ${languages[2]}, Framework: ${frameworks[2]}, Tool: ${tools[2]}`);
+console.log(`Language: ${languages[2]}, Framework: ${frameworks[0]}, Tool: ${tools[1]}`);
+console.log(`Language: ${languages[1]}, Framework: ${frameworks[0]}, Tool: ${tools[0]}`);
